a6a5623bebc5565ccb8d0df9ff3c2a774ed64369
auliaahc <auhappycahya11@gmail.com>
2024-05-04 12:16:01 +0700

    `style`: add color constant from design system

---
+++ lib/constants/color_constant.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static Color primary = const Color(0xFF5E73E1);
+  static Color secondary = const Color(0xFF263141);
+  static Color white = const Color(0XFFFDFDFD);
+  static Color gray1 = const Color(0xFFF5F7FA);
+  static Color gray2 = const Color(0xFFF2F5F9);
+  static Color gray3 = const Color(0xFFDDE3EB);
+  static Color gray4 = const Color(0xFFC3CAD4);
+  static Color gray5 = const Color(0xFFA1ABB9);
+  static Color gray6 = const Color(0xFF7E8794);
+  static Color gray7 = const Color(0xFF474F5A);
+
+  static Color successColor = const Color(0xFF30D158);
+  static Color warningColor = const Color(0xFFFF8514);
+  static Color errorColor = const Color(0xFFFF3B30);
+}
